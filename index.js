efdc0fc77bd007a33a4ae86f9f78eb0556e54a07
nirbhay <yadavnirbhay2@gmail.com>
2023-05-11 23:59:58 +0530

    value of port is now can be non static

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 8000;
+const port = process.env.PORT || 8000;
 const mongoose = require('./config/mongoose');
 const passport = require('passport');
 const passportJwt = require('./config/passport-jwt-auth');
-
-
 
 // middleware used to decode the encrypted data with the help of bodyparser 
 app.use(bodyParser.urlencoded({extended: false}));
